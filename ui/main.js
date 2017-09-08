5abcf79fee11754b5c04429d9d5420a7e3f3f79f
Praful Mishra <prafulmishra0203@gmail.com>
2017-09-08 17:12:36 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,7 @@ console.log('Loaded!');
 
 //Change image
 var img = document.getElementById('madi');
+var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft+10;
     img.style.marginLeft = marginLeft+'px';
