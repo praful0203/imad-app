3fbd989e11a6e29b42ca4352868c6b8bbec2c183
Praful Mishra <prafulmishra0203@gmail.com>
2017-09-09 17:14:12 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,12 @@
-console.log('Loaded!');
-
-//Change image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft+1;
-    img.style.marginLeft = marginLeft+'px';
-}
-img.onclick = function() {
-  var interval = setInterval(moveRight,40);
+//Counter
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function() {
+    
+    
+    
+    //Span
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
