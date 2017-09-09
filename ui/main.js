571c7941253880e30c8b1470336acdc45c9c54bd
Praful Mishra <prafulmishra0203@gmail.com>
2017-09-09 17:29:57 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,22 @@
 //Counter
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function() {
     
-    
-    
-    //Span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    //Create Request object
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+      if(request.readystate === XMLHttpRequest.DONE){
+          //Take some action
+          if(request.status === 200){
+              var counter = request.responseText;
+               var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+          }
+      }  
+      //Not yet
+      
+    };
+    //Make Request
+    request.open('GET','http://prafulmishra0203.imad.hasura-app.io/',true);
+    request.send(null);
 };
