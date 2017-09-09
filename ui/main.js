97fa1e88832c8821e147fc5c5e6d3e2ac4eb905d
Praful Mishra <prafulmishra0203@gmail.com>
2017-09-09 17:39:20 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,6 +17,6 @@ button.onclick = function() {
       
     };
     //Make Request
-    request.open('GET','http://prafulmishra0203.imad.hasura-app.io/', true);
+    request.open('GET','http://prafulmishra0203.imad.hasura-app.io/counter', true);
     request.send(null);
 };
